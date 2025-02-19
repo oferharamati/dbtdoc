5391ef995527331049d9cd84771f1ec13d269877
oferharamati <ofer@blocks.ws>
2025-02-19 17:49:53 +0200

    feat: commit first time

---
+++ a.sh
@@ -0,0 +1,7 @@
+echo "# dbtdoc" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M ofer1
+git remote add origin https://github.com/oferharamati/dbtdoc.git
+git push -u origin main
